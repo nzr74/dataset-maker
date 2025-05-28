d8e5225c2c2418304ce1bdcf724892e3e04d9ef9
nzr74 <m.theoretical@gmail.com>
2025-05-28 14:42:50 +0330

    Data directory problem fixed.

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,5 @@
 import csv
-
+import os
 
 class Savedata:
 
@@ -7,15 +7,22 @@ class Savedata:
 
         self.destination = destination
         self.fields = fields
-
+        if not os.path.exists(self.destination):
+            directory = self.destination.rsplit("/", 1)[0]
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            with open(self.destination, "w") as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=self.fields)
+                writer.writeheader()
+            print(f"Created new file: {self.destination}")
     def save_data(self, details, missing_value, site):
         full_data_size = {"divar": 13, "bama": 9}
-       
+
         def save():
             with open(self.destination, "a", newline="", encoding="utf-8") as csvfile:
                 writer = csv.DictWriter(csvfile, fieldnames=self.fields)
-                if csvfile.tell() == 0:
-                    writer.writeheader()
+                # if csvfile.tell() == 0:
+                #     writer.writeheader()
                 writer.writerow(details)
                 csvfile.close()
 
